7ed5c1b3fdc5fbaf2ae0f5aa2cb5d13ecc082a3f
Drakeor <drakeor.dragon@gmail.com>
2020-12-19 23:15:19 -0800

    Added python

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # StackOverflow OS
 This program is created as a parody to Stack Overflow anwsers, encapsulating all the mindlessly repeated phrases and dumb anwsers in the site
 
+# Instructions
+- apt-get install python2.7-dev
+- cmake .
+- make
+
+
 # Q&A
 
 ## Why?
--- main.c
+++ main.c
@@ -1,6 +1,12 @@
-#include "stdio.h"
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-    printf("test\n");
+  Py_SetProgramName(argv[0]);  /* optional but recommended */
+  Py_Initialize();
+  PyRun_SimpleString("from time import time,ctime\n"
+                     "print 'Today is',ctime(time())\n");
+  Py_Finalize();
+  return 0;
 }
