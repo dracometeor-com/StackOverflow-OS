a141607a6ca841690c6fcbba60c879c1b443ff1d
Drakeor <drakeor.dragon@gmail.com>
2020-12-20 01:34:47 -0800

    added more dumb stuff

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # StackOverflow OS
-This program is created as a parody to Stack Overflow anwsers, encapsulating all the mindlessly repeated phrases and dumb anwsers in the site
+This program is created as a parody to Stack Overflow anwsers, encapsulating all the mindlessly repeated phrases and dumb anwsers in the site. Everything in this project is based on actual highly rated anwsers and tropes in Stack Overflow. 
 
 # Instructions
 - apt-get install python2.7-dev
@@ -17,3 +17,23 @@ This topic has been closed. There is not enough information.
 ## Why can't I pass command line arguments?
 
 (+46 upvotes) Command line arguments use pointers which are BAD.
+
+## Why is there so much copy pasted code
+
+(+13 upvotes) You should avoid pre-processor macros at all costs. They lead to unmanagable code. 
+
+## Why is the sort so slow?
+
+(+54 upvotes) Bubble sort is just fine. Picking a different algorithm would be premature optimization which is the root of all evil.
+
+## Isn't it inefficient to intepret python for everything, even simple commands?
+
+(+242 upvotes) Premature optimization which is the root of all evil
+
+## Why does printing a variable use jQuery? You don't need to include jQuery in everything.
+
+(+16 upvotes) Actually you do. Why use anything else. Even if you're doing an assignment where your instructor explicitly says to not use a 3rd party library, you should do it in jQuery and my anwser will reflect that.
+
+## Why include the entire boost library to use the boost smart pointer when standard library has a perfectly functional one?
+
+(+24 upvotes) You should always use Boost whenever possible. ALWAYS. No exceptions.
--- main.cpp
+++ main.cpp
@@ -1,5 +1,49 @@
 #define PY_SSIZE_T_CLEAN
+
+#include <iostream>
 #include <Python.h>
+
+using namespace std;
+
+void help()
+{
+  Py_Initialize();
+  PyRun_SimpleString("print 'Type sort to sort a list'\n"
+                     "print 'Type printvar to print a var that has value 2'\n"
+                     "print 'Type exit to exit'\n");
+  Py_Finalize();
+}
+
+void sort()
+{
+  Py_Initialize();
+  PyRun_SimpleString("import random\n"
+                     "array = list(range(1, 1000000))\n"
+                     "random.shuffle(array)\n"
+                     "length = len(array)\n"
+                     "print 'sorting array'\n"
+                     "for i in range(0,length-1):\n"
+                     "    for j in range(0,length-i-1):\n"
+                     "        if array[j] < array[j+1]:\n"
+                     "            array[j], array[j+1] = array[j+1], array[j] \n"
+                     "print 'array sorted'\n"
+                     );
+  Py_Finalize();
+}
+
+void printvar()
+{
+  Py_Initialize();
+  PyRun_SimpleString("import re\n"
+                     "f = open(\"jquery.js\", \"r\")\n"
+                     "n = f.read()\n"
+                     "avar = '$(\\'#var\\').val( 2 );'\n"
+                     "avar = re.findall('\\d+', avar )[0]\n"
+                     "print avar\n"
+                     );
+  Py_Finalize();
+}
+
 
 int main()
 {
@@ -8,5 +52,27 @@ int main()
                      "print 'Welcome to Stack Overflow OS - Today is',ctime(time())\n"
                      "print 'Type help for help'\n");
   Py_Finalize();
+  bool isDone = false;
+
+  while(!isDone)
+  {
+    string cmd;
+    cin >> cmd;
+    if(cmd == "help") {
+      help();
+    } else if(cmd == "sort") {
+      sort();
+    } else if(cmd == "printvar") {
+      printvar();
+    } else if(cmd == "exit") {
+      isDone = true;
+    } else {
+      Py_Initialize();
+      PyRun_SimpleString("print 'unknown command. type help for help'\n");
+      Py_Finalize();
+    }
+  }
+
+
   return 0;
 }
