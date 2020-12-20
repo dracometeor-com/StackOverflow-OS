28a053837d8a532fe4bb003a9ef0763e0bf17480
Drakeor <drakeor.dragon@gmail.com>
2020-12-20 00:53:53 -0800

    Adding JQuery and Boost

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@ int main(int argc, char *argv[])
   Py_SetProgramName(argv[0]);  /* optional but recommended */
   Py_Initialize();
   PyRun_SimpleString("from time import time,ctime\n"
-                     "print 'Welcome to Stack Overflow OS - Today is',ctime(time())\n");
+                     "print 'Welcome to Stack Overflow OS - Today is',ctime(time())\n"
+                     "print 'Type help for help'\n");
   Py_Finalize();
   return 0;
 }
